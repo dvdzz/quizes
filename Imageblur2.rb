0e92a7cd7eac2b2c4dc1338cefd6766d4b7fc753
dvdzz <dvdzz76@icloud.com>
2019-06-24 23:53:54 +0000

    finished blur image 2 challange.

--- Imageblur2.rb
+++ Imageblur2.rb
@@ -1,24 +1,14 @@
-class Image 
-    attr_accessor :photo
-  
-    def initialize(photo)
-    @photo = photo
+class Image
+
+  def initialize(array)
+    @image = array
   end
 
   def output_image
-    @photo.each do |data|
-      puts data.join
+    @image.each do |row|
+      puts row.join
     end
   end
-end
-
-image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
-])
-image.output_image
 
   def blur!(distance=1)
     distance.times do
